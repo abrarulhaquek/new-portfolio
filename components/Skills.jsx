9db650a47c5e3f3adfdd4b68f6ab3f858e41c115
zydnitech <imran@zydni.com>
2023-08-28 12:53:22 +0530

    check error

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Spline from '@splinetool/react-spline';
+// import Spline from '@splinetool/react-spline';
 import Image from 'next/image';
 import { AnimatedElement } from '@/pages/animation';
 
--- pages/animation.jsx
+++ pages/animation.jsx
@@ -1,31 +1,4 @@
-// import React from 'react';
-// import { useInView } from 'react-intersection-observer';
 
-// const AnimatedBT = ({ children }) => {
-//     const [ref, inView] = useInView({
-//         triggerOnce: true,
-//     });
-//     return <div ref={ref} className={inView ? 'animated-BT in-view-BT' : 'animated-BT'}>{children}</div>;
-// };
-
-// const AnimatedLR = ({ children }) => {
-//     const [ref, inView] = useInView({
-//         triggerOnce: true,
-//     });
-
-
-//     return <div ref={ref} className={inView ? 'animated-LR in-view-LR' : 'animated-LR'}>{children}</div>;
-// };
-// const AnimatedRL = ({ children }) => {
-//     const [ref, inView] = useInView({
-//         triggerOnce: true,
-//     });
-
-
-//     return <div ref={ref} className={inView ? 'animated-RL in-view-RL' : 'animated-RL'}>{children}</div>;
-// };
-
-// export { AnimatedBT, AnimatedLR, AnimatedRL };
 import React from 'react';
 import { useInView } from 'react-intersection-observer';
 
