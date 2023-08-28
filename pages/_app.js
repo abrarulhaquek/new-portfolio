cb7f6e43809fea90d776621d2874501e93ba13f0
zydnitech <imran@zydni.com>
2023-08-28 13:09:36 +0530

    check 4

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
 import '@/styles/style.css'
-import { ThemeChanger } from './ThemeChanger'
+import { ThemeChangerProvider } from './ThemeChanger'
 
 export default function App({ Component, pageProps }) {
 
   return (
-    <ThemeChanger>
+    <ThemeChangerProvider>
       <Component {...pageProps} />
-    </ThemeChanger>
+    </ThemeChangerProvider>
   )
 }
